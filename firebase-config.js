0149603c4815de6eade9739db393649fe5d61caf
Henriquecassol <168142302+henriquecassol09@users.noreply.github.com>
2025-09-02 11:47:38 -0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,21 @@
-// Import Firebase modules
-import firebase from "firebase/app"
-import "firebase/firestore"
-import "firebase/auth"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Configuração do Firebase
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-  authDomain: "revizze-car.firebaseapp.com",
-  projectId: "revizze-car",
-  storageBucket: "revizze-car.appspot.com",
-  messagingSenderId: "xxxxxxxxxxxx",
-  appId: "1:xxxxxxxxxxxx:web:xxxxxxxxxxxxxxxx",
-}
+  apiKey: "AIzaSyAbaA2IA0y_sPx6G6jCNmg-tE4S4IYmqXg",
+  authDomain: "rc-vendas-e-instalacoes.firebaseapp.com",
+  projectId: "rc-vendas-e-instalacoes",
+  storageBucket: "rc-vendas-e-instalacoes.firebasestorage.app",
+  messagingSenderId: "324769531700",
+  appId: "1:324769531700:web:88cb4ada80c6ec82a24761",
+  measurementId: "G-TV02JKJKFZ"
+};
 
-// Inicializar Firebase
-firebase.initializeApp(firebaseConfig)
-
-// Referências para uso global
-const db = firebase.firestore()
-const auth = firebase.auth()
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
