4a58211e4f691873597069e96340613419cd9e45
Henriquecassol <168142302+henriquecassol09@users.noreply.github.com>
2025-09-09 11:37:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
-// Import Firebase modules
-import firebase from "firebase/app"
-import "firebase/firestore"
-import "firebase/auth"
 
-// Configuração do Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyBxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-  authDomain: "revizze-car.firebaseapp.com",
-  projectId: "revizze-car",
-  storageBucket: "revizze-car.appspot.com",
-  messagingSenderId: "xxxxxxxxxxxx",
-  appId: "1:xxxxxxxxxxxx:web:xxxxxxxxxxxxxxxx",
-}
-
-// Inicializar Firebase
-firebase.initializeApp(firebaseConfig)
-const db = firebase.firestore()
-const auth = firebase.auth()
-
-// Sistema de Gestão Revizze Car
-class RevizzeCarSystem {
+class RCVendasSystem {
   constructor() {
     this.produtos = []
     this.entradas = []
